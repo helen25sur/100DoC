05c7f0822accefd0f9650cb6c76a024714a72be1
Olena Surilova <elen21surilova@gmail.com>
2025-01-26 17:57:12 +0100

    js-DOM: add scripts for event project

--- js-DOM/app.js
+++ js-DOM/app.js
@@ -1,1 +1,28 @@
+const input = document.body.querySelector('#productname');
+const counter = document.body.querySelector('#counter');
+const startLengthQuery = 66;
 
+input.addEventListener('input', (evt) => {
+  console.log(evt.target.value);
+  const value = evt.target.value;
+  const length = value.length;
+  const remainedChars = startLengthQuery - length;
+  counter.innerText = remainedChars;
+  if (remainedChars <= 0) {
+    input.setAttribute('readonly', true);
+  } else if (remainedChars <= 10) {
+    input.classList.add('highlighted');
+    counter.classList.add('highlighted');
+  } else if (remainedChars > 10) {
+    input.classList.remove('highlighted');
+    counter.classList.remove('highlighted');
+  }
+
+});
+
+document.body.addEventListener('keydown', (evt) => {
+  if (evt.key == 'Backspace' || evt.key == 'Delete' && input.hasAttribute('readonly')) {
+    input.removeAttribute('readonly');
+    console.log(evt)
+  }
+})
