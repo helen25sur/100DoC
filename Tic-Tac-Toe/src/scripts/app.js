0cdff770b21788d8ee4660af2916510cc722c22c
Olena Surilova <elen21surilova@gmail.com>
2025-02-05 21:57:50 +0100

    Tic-Tac-Toe: add begining game functionality

--- Tic-Tac-Toe/src/scripts/app.js
+++ Tic-Tac-Toe/src/scripts/app.js
@@ -1,4 +1,11 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+]
+
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -15,10 +22,15 @@ const playerConfigDialogElement = document.getElementById('config-user');
 const backdropElement = document.querySelector('.backdrop');
 const formElement = document.querySelector('form');
 const errorsOutput = document.getElementById('config-errors');
+const playerTurnElement = document.getElementById('player-turn');
+const gameFieldElement = document.getElementById('game-field');
+const gameFieldItems = gameFieldElement.querySelectorAll('li.game-field-item');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const closeDialogButton = document.getElementById('close-dialog');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -29,3 +41,9 @@ backdropElement.addEventListener('click', closePlayerConfig);
 playerConfigDialogElement.addEventListener("mousedown", closePlayerConfigBackdrop);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+gameFieldItems.forEach(item => {
+  item.addEventListener('click', selectGameField);
+})
