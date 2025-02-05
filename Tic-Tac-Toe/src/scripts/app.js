b1ab2f9f0f72149aa736d5665055a5d187295de1
Olena Surilova <elen21surilova@gmail.com>
2025-02-05 14:26:04 +0100

    Tic-Tac-Toe: add js for configurate players

--- Tic-Tac-Toe/src/scripts/app.js
+++ Tic-Tac-Toe/src/scripts/app.js
@@ -1,5 +1,20 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: '',
+    symbol: 'X'
+  },
+  {
+    name: '',
+    symbol: 'O'
+  }
+];
+
 const playerConfigDialogElement = document.getElementById('config-user');
 const backdropElement = document.querySelector('.backdrop');
+const formElement = document.querySelector('form');
+const errorsOutput = document.getElementById('config-errors');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
@@ -11,9 +26,6 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 closeDialogButton.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
-playerConfigDialogElement.addEventListener("mousedown", (event) => {
-  if (event.target === playerConfigDialogElement) {
-    backdropElement.style.display = 'none';
-    playerConfigDialogElement.close();
-  }
-});
+playerConfigDialogElement.addEventListener("mousedown", closePlayerConfigBackdrop);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- Tic-Tac-Toe/src/scripts/config.js
+++ Tic-Tac-Toe/src/scripts/config.js
@@ -1,24 +1,39 @@
-
-
-function openPlayerConfig() {
+function openPlayerConfig(evt) {
+  const clickedButton = evt.target;
+  editedPlayer = +clickedButton.dataset.playerid;
   playerConfigDialogElement.showModal();
   backdropElement.style.display = 'block';
 }
 
-function closePlayerConfig(evt) {
-  console.log(evt.target);
+function closePlayerConfig() {
   playerConfigDialogElement.close();
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('remove');
+  errorsOutput.textContent = '';
+  formElement.querySelector('input').value = '';
 }
 
 function closePlayerConfigBackdrop(evt) {
-  const dialogRect = dialog.getBoundingClientRect();
-  if (
-    evt.clientX < dialogRect.left ||
-    evt.clientX > dialogRect.right ||
-    evt.clientY < dialogRect.top ||
-    evt.clientY > dialogRect.bottom
-  ) {
-    playerConfigDialogElement.close(); // ✅ Закриває модалку
+  if (evt.target === playerConfigDialogElement) {
+    closePlayerConfig();
   }
+}
+
+function savePlayerConfig(evt) {
+  evt.preventDefault();
+
+  const formData = new FormData(evt.target);
+  const enteredPlayerName = formData.get('playername').trim();
+
+  if (!enteredPlayerName) {
+    evt.target.firstElementChild.classList.add('error');
+    errorsOutput.textContent = 'Please enter a valid name!';
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(`player-${editedPlayer}-data`);
+  updatedPlayerDataElement.querySelector('h3').innerText = enteredPlayerName;
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
