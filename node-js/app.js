d03d5a907348af7839f7393f870c19d73a3d0232
Olena Surilova <elen21surilova@gmail.com>
2025-03-09 18:56:08 +0100

    node-js: read form data

--- node-js/app.js
+++ node-js/app.js
@@ -1,12 +1,15 @@
 // import package
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // create app
 const app = express();
 
 // middleware for static files into public folder
 app.use(express.static('public'));
+// for reading and writing into files
+app.use(express.urlencoded({extended: false}));
 
 // 1. Create route to '/' path
 app.get('/', (req, res) => {
@@ -22,6 +25,18 @@ app.get('/restaurants', (req, res) => {
 app.get('/recommend', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'recommend.html');
   res.sendFile(filePath);
+});
+
+app.post('/recommend', (req, res) => {
+  const restaurant = req.body;
+  const pathFile = path.join(__dirname, 'data', 'restaurants.json');
+  const fileData = fs.readFileSync(pathFile);
+  const arrData = JSON.parse(fileData);
+  arrData.push(restaurant);
+  const textData = JSON.stringify(arrData);
+  fs.writeFileSync(pathFile, textData);
+
+  res.redirect('/confirm');
 });
 
 app.get('/about', (req, res) => {
